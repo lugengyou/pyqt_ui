db7972c9ae8bb6b51f546fe07e5ce0343f44b105
gengyou.lu <1770591868@qq.com>
2024-11-18 16:54:37 +0800

    [NEW] 添加历史记录显示功能

--- tbps_ui/ui_designer/TbpsUiMainWindow.py
+++ tbps_ui/ui_designer/TbpsUiMainWindow.py
@@ -2,12 +2,13 @@
 Author: gengyou.lu 1770591868@qq.com
 Date: 2024-11-12 10:37:08
 FilePath: /tbps_ui/ui_designer/TbpsUiMainWindow.py
-LastEditTime: 2024-11-13 17:26:12
+LastEditTime: 2024-11-18 16:29:18
 Description: tbps ui 
 '''
-import sys
+import os
 import json
 import socket
+import shutil
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QGraphicsScene
 from .Ui_tbps import Ui_MainWindow 
@@ -32,6 +33,7 @@ class MyMainWindow(QMainWindow,Ui_MainWindow):
                                     self.label_show_p6, self.label_show_p7, self.label_show_p8, self.label_show_p9, self.label_show_p10]
         self.clientSocket = None
         self.inferResult = None
+        self.currentShow = None
 
     def slot_connectServer(self):
         ip = self.lineEdit_ip.text()
@@ -44,7 +46,7 @@ class MyMainWindow(QMainWindow,Ui_MainWindow):
             self.clientSocket.connect((ip, int(port)))
             self.textBrowser_output.append("connect server success.") 
         except socket.error as msg:
-            self.textBrowser_output.append(msg)            
+            self.textBrowser_output.append(str(msg))            
 
     def slot_clearInput(self):
         self.plainTextEdit_text.clear()
@@ -61,27 +63,48 @@ class MyMainWindow(QMainWindow,Ui_MainWindow):
         message = json.dumps(message)
         self.clientSocket.send(message.encode())
         self.textBrowser_output.append("send message success.")
-        # 等待搜索结果
-        response = self.clientSocket.recv(1024).decode()
-        self.textBrowser_output.append(f"Received from server: {response}")        
-        self.inferResult = json.loads(response)
+        # 等待搜索结果        
+        self.receiveAndSaveResult()
 
     def slot_display(self):        
-        search_result = self.readJsonFile('result/search_result.json')        
-        if (len(search_result) == 0):            
-            return        
+        # 获取显示设置
         display_setting = self.comboBox_display.currentText()
+        display_history = self.comboBox_history.currentText()        
+        # 清空显示
+        self.clearDisplayScene(display_setting)
+        # 显示搜索结果
         if (display_setting == "Top1"):
             self.textBrowser_output.append("showing top1 result.")
-            self.displayResultSample(search_result, 1)
+            self.displayResultSample(1, display_history)
         elif (display_setting == "Top5"):
             self.textBrowser_output.append("showing top5 result.")
-            self.displayResultSample(search_result, 5)
+            self.displayResultSample(5, display_history)
         elif (display_setting == "Top10"):
             self.textBrowser_output.append("showing top10 result.")
-            self.displayResultSample(search_result, 10)
+            self.displayResultSample(10, display_history)
         else:
             self.textBrowser_output.append("clear display scene.")
+            
+    # ************************ utils functions ************************ #
+    def clearDisplayScene(self, display_setting=None): 
+        # 清空显示  
+        if (display_setting == "Top1"):
+            for i in range(1, 10):
+                self.displaySceneList[i].clear()
+                self.displayIdLabelList[i].clear()
+                self.displayShowIdList[i].clear()
+                self.displayProbLabelList[i].clear()
+                self.displayShowProbList[i].clear()
+        elif (display_setting == "Top5"):
+            for i in range(5, 10):
+                self.displaySceneList[i].clear()
+                self.displayIdLabelList[i].clear()
+                self.displayShowIdList[i].clear()
+                self.displayProbLabelList[i].clear()
+                self.displayShowProbList[i].clear()
+        elif (display_setting == "Top10"):
+            pass
+        else:                 
             for i in range(10):
                 self.displaySceneList[i].clear()
                 self.displayIdLabelList[i].clear()
@@ -89,17 +112,30 @@ class MyMainWindow(QMainWindow,Ui_MainWindow):
                 self.displayProbLabelList[i].clear()
                 self.displayShowProbList[i].clear()
 
+    def displayResultSample(self, num, display_history):  
+        # 判断是否有搜索结果
+        save_folder = f"search_result/{display_history}"
+        if not os.path.exists(save_folder):
+            self.clearDisplayScene()
+            self.textBrowser_output.append("No result found")
+            return
 
-    # ************************ utils functions ************************ #
-    def displayResultSample(self, search_result, num):
-        # data = search_result['search_result']
+        # 如果当前显示的历史记录不是当前选择的历史记录，则重新读取json文件
+        if self.currentShow != display_history:
+            self.inferResult = self.readJsonFile(f"search_result/{display_history}/search_result.json")
+        self.currentShow = display_history        
+
+        # 显示搜索结果
         data = self.inferResult['search_result']
+        input = self.inferResult['input']
+        self.textBrowser_currentDisplayInput.clear()
+        self.textBrowser_currentDisplayInput.append(input)
         for i in range(num):
             id = data[i]['id']
             prob = data[i]['probability']
             dataset = data[i]['dataset']
             filename = data[i]['filename']
-            image_path = f"dataset/{dataset}/{filename}"
+            image_path = f"{save_folder}/{dataset}/{filename}"
             image = QImage(image_path)
             self.displaySceneList[i].addPixmap(QPixmap.fromImage(image))
             self.graphicsViewList[i].setScene(self.displaySceneList[i])
@@ -110,10 +146,72 @@ class MyMainWindow(QMainWindow,Ui_MainWindow):
             self.displayShowProbList[i].setText(str(prob))
 
     def readJsonFile(self, json_file):
+        # 读取 json 文件
         with open(json_file, 'r') as f:
-            data = json.load(f)
-        # self.textBrowser_output.append(json.dumps(data))
+            data = json.load(f)        
         if (len(data) == 0):
             self.textBrowser_output.append("No result found")            
         return data
 
+    def receiveAndSaveResult(self):
+        try:
+            # 接收 json 数据                    
+            response = self.clientSocket.recv(1024).decode()              
+            # 检查 response 是否为空
+            if not response:
+                self.textBrowser_output.append("Received empty response")
+                return                 
+            # 解析 JSON
+            try:
+                json_result = json.loads(response)
+                self.currentShow = "current"
+                self.inferResult = json_result
+            except json.JSONDecodeError as e:
+                self.textBrowser_output.append(f"JSON decode error: {str(e)}")
+                return
+            
+            data = json_result['search_result']  
+            
+            save_folder = "search_result/current"
+            # 修改历史记录文件夹名
+            if os.path.exists(save_folder):                                    
+                for i in range(6, 1, -1):
+                    old_name = f"search_result/last_{i-1}"
+                    new_name = f"search_result/last_{i}"
+                    if os.path.exists("search_result/last_6"):
+                        # 删除最早的历史记录
+                        shutil.rmtree("search_result/last_6")
+                    if os.path.exists(old_name):
+                        os.rename(old_name, new_name)
+                os.rename("search_result/current", "search_result/last_1")             
+            
+            # 创建存储当前搜索结果文件夹                   
+            save_folder = f"search_result/current/{data[0]['dataset']}"
+            os.makedirs(save_folder)                
+            
+            # 保存 json 数据
+            with open(f"search_result/current/search_result.json", 'w') as f:
+                json.dump(json_result, f, indent=4, ensure_ascii=False)
+
+            # 接收图片数据并保存
+            for i in range(10):
+                # Receive the size of the image
+                image_size_data = self.clientSocket.recv(4)
+                if not image_size_data:
+                    break
+                image_size = int.from_bytes(image_size_data, 'big')
+                # Receive the image data
+                image_data = b''
+                while len(image_data) < image_size:
+                    packet = self.clientSocket.recv(image_size - len(image_data))
+                    if not packet:
+                        break
+                    image_data += packet
+                # Save the image to the specified folder
+                filename = data[i]['filename']
+                image_path = os.path.join(save_folder, filename)
+                with open(image_path, 'wb') as img_file:
+                    img_file.write(image_data)            
+        except Exception as e:
+            self.textBrowser_output.append(f"Error receiving data: {str(e)}")
+            self.textBrowser_output.append(f"Please connect to the server again.")    
